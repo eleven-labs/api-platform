ef521810dd6b89ac27356e114cdd9c6fea16e744
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2018-06-20 11:43:31 +0200

    feat: config extension

---
+++ symfony/src/Doctrine/ArticleExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Article;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class ArticleExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    /**
+     *
+     * @param QueryBuilder $queryBuilder
+     * @param string       $resourceClass
+     */
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+        if (Article::class === $resourceClass) {
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder->andWhere(sprintf('%s.id = :id', $rootAlias));
+            $queryBuilder->setParameter('id', 2);
+        }
+    }
+}
